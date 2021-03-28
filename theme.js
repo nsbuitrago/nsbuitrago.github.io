a288d2a380bbb6d1283e8e7289a05d32804ee4d8
nsbuitrago <nsbuitrago@icloud.com>
2021-03-27 20:53:28 -0400

    updated css/js

--- theme.js
+++ theme.js
@@ -1,10 +1,10 @@
 function getStyleSheet() {
     var currentTime = new Date().getHours();
-    if (6 <= currentTime&&currentTime < 21) {
-        document.write("<link rel='stylesheet' href='css/screen-light.css'></link>")
+    if (6 <= currentTime&&currentTime < 19) {
+        document.documentElement.setAttribute('theme', 'light');
     }
     else {
-        document.write("<link rel='stylesheet' href='css/screen-dark.css'></link>")
+        document.documentElement.setAttribute('theme', 'dark');
     }
 }
 
