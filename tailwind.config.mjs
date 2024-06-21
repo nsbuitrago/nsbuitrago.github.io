4a393156b52eefd0e7283c7af882423ad2269b4a
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-06-21 16:04:32 -0500

    change: move to astro site

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['PPObjectSans-Regular', 'Helvetica', 'sans-serif', 'system-ui'],
+			mono: ['JetBrainsMono[wght]', 'monospace', 'system-ui'],
+			display: ['MajorMonoDisplayNSBRegular', 'Helvetica', 'sans-serif', 'system-ui']
+		},
+		colors: {
+			'light': '#F0F1EF',
+			'dark': '#1A2023',
+			'text-dark': '#33414A',
+			'text-light': '#F0F1EF',
+			'primary': '#689689',
+			'fg-01': '#CDD8DF',
+			'fg-02': '#96A5AE',
+			'fg-03': '#5B6B75',
+			'fg-04': '#D4CAC0',
+			'fg-05': '#B2ADA4',
+			'fg-06': '#928F8A',
+		}
+	},
+	plugins: [],
+}
