6952202f70823bdcef37ab9e516c22915658556e
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-06-21 22:48:03 -0500

    fix logo typography
    fix: use typography as paths in logo
    change: media queries for logo size
    change: extend tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,25 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+			colors: {
+				'light': '#F0F1EF',
+				'dark': '#1A2023',
+				'fg-dark': '#34424C',
+				'fg-light': '#F0F1EF',
+				'primary': '#689689',
+				'fg-01': '#CDD8DF',
+				'fg-02': '#96A5AE',
+				'fg-03': '#5B6B75',
+				'fg-04': '#D4CAC0',
+				'fg-05': '#B2ADA4',
+				'fg-06': '#928F8A',
+			}
+		},
 		fontFamily: {
 			sans: ['PPObjectSans-Regular', 'Helvetica', 'sans-serif', 'system-ui'],
 			mono: ['JetBrainsMono[wght]', 'monospace', 'system-ui'],
 			display: ['MajorMonoDisplayNSBRegular', 'Helvetica', 'sans-serif', 'system-ui']
 		},
-		colors: {
-			'light': '#F0F1EF',
-			'dark': '#1A2023',
-			'fg-dark': '#34424C',
-			'fg-light': '#F0F1EF',
-			'primary': '#689689',
-			'fg-01': '#CDD8DF',
-			'fg-02': '#96A5AE',
-			'fg-03': '#5B6B75',
-			'fg-04': '#D4CAC0',
-			'fg-05': '#B2ADA4',
-			'fg-06': '#928F8A',
-		}
 	},
 	plugins: [],
 }
